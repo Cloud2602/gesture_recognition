0f11220154b794f6e2f7ca539017704cf233377b
Francesco Passiatore <126468556+FrancescoPassiatore@users.noreply.github.com>
2025-05-26 22:41:05 +0200

    Update train.py

--- train.py
+++ train.py
@@ -6,39 +6,51 @@ from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Dropout
 from tensorflow.keras.utils import to_categorical
 
+def normalize_hand(landmarks):
+    wrist = landmarks[0]
+    landmarks -= wrist
+    scale = np.linalg.norm(landmarks[9] - wrist)  # indice MCP
+    return landmarks / scale if scale > 0 else landmarks
+
 # === Config ===
-DATA_PATH = "data_static"  # cartella con sottocartelle per ogni gesto
+DATA_PATH = "/content/gesture_recognition/dataset_landmarks_augumantation"  # cartella con sottocartelle per ogni gesto
 LABELS = ["0","1", "2", "3"] #0:stop 
 NUM_FEATURES = 126  # 21 punti * 3 coordinate * 2 mani
 
 # === Caricamento dati ===
-X, y = [], []
+X, Y = [], []
 
-for label_index, label in enumerate(LABELS):
-    folder = os.path.join(DATA_PATH, label)
-    for file in os.listdir(folder):
-        if file.endswith(".npy"):
-            frame = np.load(os.path.join(folder, file))  # shape (126,) #funziona solo se ci sono 126 valori quindi da capire il num di feature
-            if frame.shape == (NUM_FEATURES,):
-                X.append(frame)
-                y.append(label_index)
 
-X = np.array(X)    #quindi questo dovrebbe essere una lista di 280 array (70x4) ognuno contenente 126 elementi
+
+for file_name in os.listdir(DATA_PATH):
+    if file_name.endswith(".npy"):
+        # (490,(42,3) )
+        file_path=os.path.join(DATA_PATH, file_name)
+        frame = np.load(file_path)  
+        for sample in frame:
+            sample = normalize_hand(sample)
+            sample= sample.reshape(-1)
+            X.append(sample)
+            Y.append(int(file_name.split(".")[0]))
+                
+
+X = np.array(X)  #(490,42)  quindi questo dovrebbe essere una lista di 280 array (70x4) ognuno contenente 126 elementi
 #formato categorical perchè usiamo categorical loss
-y = to_categorical(np.array(y), num_classes=len(LABELS))
+Y = to_categorical(np.array(Y), num_classes=len(LABELS))
+
 
 # === Preprocessing ===
 scaler = StandardScaler()      #normalizzazione -> standardizzazione z-score
 X = scaler.fit_transform(X)
 
 # === Train/test split ===
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.2, random_state=42)
 
 # === Modello MLP ===
 model = Sequential([
-    Dense(256, activation='relu', input_shape=(NUM_FEATURES,)),
+    Dense(64, activation='relu', input_shape=(NUM_FEATURES,)),
     Dropout(0.4),
-    Dense(128, activation='relu'),
+    Dense(32, activation='relu'),
     Dropout(0.3),
     Dense(len(LABELS), activation='softmax')
 ])
@@ -51,7 +63,9 @@ history = model.fit(X_train, y_train, epochs=40, batch_size=16, validation_split
 # === Valutazione ===
 loss, acc = model.evaluate(X_test, y_test)
 print(f"\n✅ Accuracy test: {acc:.2f}")
-
+y_pred = model.predict(X_test)
+cm = confusion_matrix(np.argmax(y_test, axis=1), np.argmax(y_pred, axis=1))
+ConfusionMatrixDisplay(cm).plot()
 # === Salva modello e scaler ===
 model.save("mlp_static_gesture_model.h5")
 np.save("scaler_mean.npy", scaler.mean_)
