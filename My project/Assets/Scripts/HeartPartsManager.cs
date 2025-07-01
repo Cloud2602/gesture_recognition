7b5300824e38398f6c0a6b5a9358a459880d5240
FRANCESCO PASSIATORE s332821 <s332821@studenti.polito.it>
2025-07-01 14:28:09 +0200

    Added Toggle

---
+++ My project/Assets/Scripts/HeartPartsManager.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HeartPartsManager : MonoBehaviour
+{
+    [Header("Riferimenti alle parti del cuore")]
+    public GameObject aorta;
+    public GameObject leftAtrium;
+    public GameObject leftVentricle;
+    public GameObject pulmonaryArtery;
+    public GameObject rightAtrium;
+    public GameObject rightVentricle;
+    public GameObject venaCava;
+
+    [Header("Toggle UI")]
+    public Toggle toggleAorta;
+    public Toggle toggleLeftAtrium;
+    public Toggle toggleLeftVentricle;
+    public Toggle togglePulmonaryArtery;
+    public Toggle toggleRightAtrium;
+    public Toggle toggleRightVentricle;
+    public Toggle toggleVenaCava;
+
+    void Start()
+    {
+        // Collega eventi toggle → visibilità
+        toggleAorta.onValueChanged.AddListener((val) => aorta.SetActive(val));
+        toggleLeftAtrium.onValueChanged.AddListener((val) => leftAtrium.SetActive(val));
+        toggleLeftVentricle.onValueChanged.AddListener((val) => leftVentricle.SetActive(val));
+        togglePulmonaryArtery.onValueChanged.AddListener((val) => pulmonaryArtery.SetActive(val));
+        toggleRightAtrium.onValueChanged.AddListener((val) => rightAtrium.SetActive(val));
+        toggleRightVentricle.onValueChanged.AddListener((val) => rightVentricle.SetActive(val));
+        toggleVenaCava.onValueChanged.AddListener((val) => venaCava.SetActive(val));
+    }
+}
